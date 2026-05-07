4901d4ac9cf3590a5e6dc6dfc6a1e6783e605549
fedemikhail <fedemikhail@gmail.com>
2026-05-07 15:30:39 -0600

    Create LoginPage.ts

---
+++ pages/LoginPage.ts
@@ -0,0 +1,14 @@
+import { Page } from '@playwright/test';
+import { BasePage } from './BasePage';
+
+export class LoginPage extends BasePage {
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async login(username: string, password: string): Promise<void> {
+    await this.page.getByLabel('Username').fill(username);
+    await this.page.getByLabel('Password').fill(password);
+    await this.page.getByRole('button', { name: 'Login' }).click();
+  }
+}
